50fa09802be1b732a7c47132d0c8e0f46191fc68
charissayj <charissa.y.johnson@gmail.com>
2017-09-16 12:25:36 -0500

    Angular examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  switch: boolean = true;
+  color = 'red';
+  loggedIn = true;
+  numbers = [1,2,3,4,5];
+  title = 'Dojo Mail';
+  name = "Banana";
+  user = {
+  	firstName: "Darth",
+  	lastName: "Vader"
+  }
+  number = 10;
+  amount = 1.20;
+  today = new Date();
 }
